0aacc606b6e4b5766b2de9f1a679d6478559036e
Gois <guilhermejcgois@outlook.com>
2026-06-05 16:26:51 -0300

    feat(nvim): adds neo-tree plugin
    Added Neo-tree plugin with dependencies.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -77,5 +77,17 @@ return {
       require("gitsigns").setup({})
     end,
   },
+
+  -- Neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  }
 }
 
